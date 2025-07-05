bb5f82ffdcf8eaa6fcd7b97a677e35794449765a
Firagos <jemalfiragos@gmail.com>
2025-07-05 09:44:17 +0300

    adding the bagsnag file manualy

---
+++ bugsnag.js
@@ -0,0 +1,6 @@
+// bugsnag.js
+import Bugsnag from "@bugsnag/expo";
+
+export const bugsnag = Bugsnag.start({
+  apiKey: "0aef0c66905ff898d36dbdd6b536675c", // Replace with your actual Bugsnag API key
+});
