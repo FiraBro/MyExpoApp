7e9ab66f2335ec7fba79c5f899a909fb92f2ce06
Firagos <jemalfiragos@gmail.com>
2025-07-05 21:02:35 +0300

    adding button componenets

---
+++ src/components/AppButton.js
@@ -0,0 +1,28 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import colors from "../config/colors";
+
+export default function AppButton({ tittle, onPress, color = "primaryColor" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{tittle}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+  },
+});
