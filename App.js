1e06d7aa5139164622b39a021067c2b85918646f
Firagos <jemalfiragos@gmail.com>
2025-07-07 10:50:45 +0300

    testint Accountscreen

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
-import ListDetail from "./src/Screen/ListDetail";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Screen from "./src/components/Screen";
+import AccountScreen from "./src/Screen/AccountScreen";
 
 export default function App() {
-  return <ListDetail />;
+  return (
+    <GestureHandlerRootView>
+      <Screen style={{ flex: 1 }}>
+     
+        <AccountScreen />
+      </Screen>
+    </GestureHandlerRootView>
+  );
 }
