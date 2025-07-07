6de4d52ead953a86ed46b2986458bbf8588a7afb
Firagos <jemalfiragos@gmail.com>
2025-07-07 15:52:08 +0300

    simple modification

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,12 +3,12 @@ import React from "react";
 import constants from "expo-constants";
 export default function Screen({ children, style }) {
   return (
-    <SafeAreaView style={[style.screen, style]}>
+    <SafeAreaView style={[styles.screen, style]}>
       <View style={style}>{children}</View>
     </SafeAreaView>
   );
 }
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   screen: {
     paddingTop: constants.statusBarHeight,
   },
