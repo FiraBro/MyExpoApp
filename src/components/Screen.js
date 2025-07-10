16432f6adb514eb5491c31f7ef3c11eda9dc1e6c
Firagos <jemalfiragos@gmail.com>
2025-07-10 09:48:18 +0300

    adding flex to one

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,12 +4,15 @@ import constants from "expo-constants";
 export default function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>{children}</View>
+      <View style={[styles.flex, style]}>{children}</View>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   screen: {
     paddingTop: constants.statusBarHeight,
+  },
+  view: {
+    flex: 1,
   },
 });
