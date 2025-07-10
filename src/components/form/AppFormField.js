055f8c254c37a2e2fc369cc9acb2133e246e29db
Firagos <jemalfiragos@gmail.com>
2025-07-10 10:38:58 +0300

    simple modification

--- src/components/form/AppFormField.js
+++ src/components/form/AppFormField.js
@@ -3,7 +3,12 @@ import { useFormikContext } from "formik";
 import AppInputText from "../AppInputText";
 import ErrorMessage from "./ErrorMessage";
 
-export default function AppFormField({ name, width, ...otherProps }) {
+export default function AppFormField({
+  name,
+  width,
+  PickerItemComponent,
+  ...otherProps
+}) {
   const { handleBlur, touched, handleChange, errors, values } =
     useFormikContext();
 
@@ -12,6 +17,7 @@ export default function AppFormField({ name, width, ...otherProps }) {
       <AppInputText
         onChangeText={handleChange(name)}
         onBlur={handleBlur(name)}
+        PickerItemComponent
         value={values[name]}
         {...otherProps}
         width={width}
