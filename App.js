0d03db659ba6764393fa2c1c2f08900e07ba393b
Firagos <jemalfiragos@gmail.com>
2025-07-11 10:39:52 +0300

    testing the Apppimageinput components

--- App.js
+++ App.js
@@ -1,38 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
-import { View, Text, Alert, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import AppImageInput from "./src/components/AppImageInput";
 import Screen from "./src/components/Screen";
 import AppButton from "./src/components/AppButton";
-
 export default function App() {
-  const [imageUrl, setImageUrl] = useState();
-  useEffect(() => {
-    (async () => {
-      const { status } =
-        await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== "granted") {
-        Alert.alert("Permission Required", "Camera access is needed.");
-      }
-    })();
-  }, []);
+  const [imageUri, setImageUri] = useState(null);
 
-  const selectImage = async () => {
+  const select = async () => {
     try {
       const result = await ImagePicker.launchImageLibraryAsync();
-      const imageUri = result.uri;
-      setImageUrl(imageUri);
+      if (!result.canceled) setImageUri(result.assets[0].uri);
     } catch (error) {
-      console.log("some thing went wrong", error);
+      console.log(error);
     }
   };
 
   return (
-    <Screen>
-      <AppButton title="Select" onPress={selectImage} />
-      <Image
-        source={{ uri: imageUrl }}
-        style={{ flex: 1, width: 200, height: 200 }}
-      />
+    <Screen style={styles.screen}>
+      <AppButton title="select" onPress={select} />
+      <AppImageInput imageUri={imageUri} />
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
