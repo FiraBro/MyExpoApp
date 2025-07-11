5bca93d28f18cd13573012681c351b9bb342a356
Firagos <jemalfiragos@gmail.com>
2025-07-11 09:46:52 +0300

    testing image picker

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { View, Text, Alert, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import Screen from "./src/components/Screen";
-import LoginScreen from "./src/Screen/LoginScreen";
-import ListingEditScreen from "./src/Screen/ListingEditScreen";
+import AppButton from "./src/components/AppButton";
 
 export default function App() {
+  const [imageUrl, setImageUrl] = useState();
+  useEffect(() => {
+    (async () => {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission Required", "Camera access is needed.");
+      }
+    })();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      const imageUri = result.uri;
+      setImageUrl(imageUri);
+    } catch (error) {
+      console.log("some thing went wrong", error);
+    }
+  };
+
   return (
     <Screen>
-      {/* <LoginScreen /> */}
-      <ListingEditScreen />
+      <AppButton title="Select" onPress={selectImage} />
+      <Image
+        source={{ uri: imageUrl }}
+        style={{ flex: 1, width: 200, height: 200 }}
+      />
     </Screen>
   );
 }
