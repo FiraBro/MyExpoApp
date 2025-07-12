3c2f2dd7cccf2006d2a8ffe31e9654b6b80050ee
Firagos <jemalfiragos@gmail.com>
2025-07-12 09:16:59 +0300

    implementing touch

--- src/components/AppImageInput.js
+++ src/components/AppImageInput.js
@@ -1,37 +1,55 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
-import AppButton from "../components/AppButton";
-import * as ImagePicker from "expo-image-picker";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import * as ImagePicker from "expo-image-picker";
 import colors from "../config/colors";
 
-export default function AppImageInput({ imageUri }) {
-  const cancelSelect = async () => {
-    try {
-      setImageUri(null);
-    } catch (error) {
-      console.log(error);
+export default function AppImageInput({ imageUri, onChangeImage }) {
+  const handlePress = async () => {
+    if (!imageUri) select();
+    else {
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
     }
   };
+
+  const select = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      onChangeImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons
-          name="camera"
-          size={35}
-          style={{ color: colors.medium }}
-        />
-      )}
-      {imageUri && (
-        <Pressable onPress={cancelSelect}>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={35}
+            style={{ color: colors.medium }}
+          />
+        )}
+        {imageUri && (
           <Image
             source={{ uri: imageUri }}
             style={{ width: 200, height: 200, marginTop: 10 }}
-            onPress={cancelSelect}
           />
-        </Pressable>
-      )}
-    </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -44,9 +62,5 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
     borderRadius: 10,
     overflow: "hidden",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
   },
 });
