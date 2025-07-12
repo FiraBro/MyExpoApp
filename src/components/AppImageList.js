3caf4efec7cbc00e270600b506b14d815ba50091
Firagos <jemalfiragos@gmail.com>
2025-07-12 10:52:31 +0300

    implementing auto scroll to the end

--- src/components/AppImageList.js
+++ src/components/AppImageList.js
@@ -1,5 +1,5 @@
-import { View, StyleSheet, Text } from "react-native";
-import React from "react";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import React, { useRef } from "react";
 import AppImageInput from "./AppImageInput";
 
 export default function AppImageList({
@@ -7,18 +7,29 @@ export default function AppImageList({
   onAddImage,
   onRemoveImage,
 }) {
+  const scrollView = useRef();
   return (
-    <View style={styles.container}>
-      <View style={styles.image}>
-        {imageUris.map((uri) => (
-          <AppImageInput
-            imageUri={uri}
-            key={uri}
-            onChangeImage={() => onRemoveImage(uri)}
-          />
-        ))}
-      </View>
-      <AppImageInput onChangeImage={(uri) => onAddImage(uri)} />
+    <View>
+      <ScrollView
+        horizontal
+        ref={scrollView}
+        onContentSizeChange={() =>
+          scrollView.current.scrollToEnd({ animated: true })
+        }
+      >
+        <View style={styles.container}>
+          <View style={styles.image}>
+            {imageUris.map((uri) => (
+              <AppImageInput
+                imageUri={uri}
+                key={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            ))}
+          </View>
+          <AppImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
