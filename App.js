8009ce666dcc614dd2a9a792b5c653e83bf6b1d1
Firagos <jemalfiragos@gmail.com>
2025-07-13 09:53:58 +0300

    trying the navigation stack

--- App.js
+++ App.js
@@ -1,9 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
-import ListingEditScreen from "./src/Screen/ListingEditScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Screen from "./src/components/Screen"; // Capitalized
+
+const Stack = createStackNavigator(); // Fixed typo
+
+const Tweets = () => {
+  return (
+    <Screen>
+      <View>
+        <Text>Tweets</Text>
+      </View>
+    </Screen>
+  );
+};
+
+const TweetsDetail = () => {
+  return (
+    <Screen>
+      <View>
+        <Text>TweetsDetail</Text>
+      </View>
+    </Screen>
+  );
+};
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="TweetsDetail" component={TweetsDetail} />
+      <Stack.Screen name="Tweets" component={Tweets} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({});
