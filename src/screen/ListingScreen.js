eafe9f3fb1faff0e5dc6c59f20b59d47457e1e1d
Firagos <jemalfiragos@gmail.com>
2025-07-14 11:05:24 +0300

    adding error to it

--- src/screen/ListingScreen.js
+++ src/screen/ListingScreen.js
@@ -4,23 +4,32 @@ import Screen from "../components/Screen";
 import colors from "../config/colors";
 import Card from "../components/Card";
 import Listing from "../api/Listing";
+import AppTexts from "../components/AppTexts";
+import AppButton from "../components/AppButton";
 
 export default function ListingScreen() {
   const [listing, setListing] = useState([]);
+  const [error, setError] = useState(false);
+
+  const getListings = async () => {
+    const response = await Listing();
+    console.log("Response:", response.data);
+    if (response.ok) setListing(response.data);
+    else setError(response.problem);
+  };
 
   useEffect(() => {
-    const getListings = async () => {
-      const response = await Listing();
-      console.log("Response:", response.data);
-      if (response.ok) setListing(response.data);
-      else console.warn("API failed:", response.problem);
-    };
-
     getListings();
   }, []);
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppTexts>Cannot Find Listing Items.</AppTexts>
+          <AppButton title="Retry" onPress={getListings} />
+        </>
+      )}
       <FlatList
         data={listing}
         keyExtractor={(item) => item.id.toString()}
