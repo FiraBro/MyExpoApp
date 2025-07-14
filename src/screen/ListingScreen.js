e4a82badb047f902556dba351f8d1059f1beb229
Firagos <jemalfiragos@gmail.com>
2025-07-14 10:25:57 +0300

    fteching data from backend

--- src/screen/ListingScreen.js
+++ src/screen/ListingScreen.js
@@ -1,34 +1,34 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 import Card from "../components/Card";
+import Listing from "../api/Listing";
 
-const listingItem = [
-  {
-    title: "Fira jacket",
-    id: 1,
-    image: require("../../assets/Logo.jpg"),
-    price: 100,
-  },
-  {
-    title: "Fira jacket",
-    id: 2,
-    image: require("../../assets/Logo.jpg"),
-    price: 1000,
-  },
-];
 export default function ListingScreen() {
+  const [listing, setListing] = useState([]);
+
+  useEffect(() => {
+    const getListings = async () => {
+      const response = await Listing();
+      console.log("Response:", response.data);
+      if (response.ok) setListing(response.data);
+      else console.warn("API failed:", response.problem);
+    };
+
+    getListings();
+  }, []);
+
   return (
     <Screen style={styles.screen}>
       <FlatList
-        data={listingItem}
-        keyExtractor={(item) => item.id}
+        data={listing}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <Card
-            tittle={item.title}
+            title={item.title}
             subTittle={"$" + item.price}
-            image={item.image}
+            image={item.images[0]}
           />
         )}
       />
