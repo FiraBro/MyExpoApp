8b16b7653bc0a4348895e6c2dc617b84c490564d
Firagos <jemalfiragos@gmail.com>
2025-07-17 10:24:06 +0300

    adding cache to it

--- src/api/Client.js
+++ src/api/Client.js
@@ -1,5 +1,22 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 export default api = create({
   baseURL: "http://192.168.1.103:5000",
 });
+
+// Keep reference to the original GET
+const originalGet = api.get;
+
+// Override GET
+api.get = async (url, params, axiosConfig) => {
+  const response = await originalGet(url, params, axiosConfig);
+
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  const cachedData = await cache.get(url);
+  return cachedData ? { ok: true, data: cachedData } : response;
+};
